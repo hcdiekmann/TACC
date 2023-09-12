69e5ff76aeccbaa2b8c5c11ec2d5f5b4bd5efe19
hcdiekmann <hcdiekmann@hotmail.com>
2023-09-12 12:57:56 +1200

    add navbar template

--- tacc-app/src/pages/HomePage.tsx
+++ tacc-app/src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
 import { Title } from '@mantine/core';
+import { MainNavbar } from '../components/Navbar';
 
 export const HomePage = (): JSX.Element => {
-  return <Title order={1}>Home</Title>;
+  return (
+    <>
+      <MainNavbar />
+      <Title order={1}>Home</Title>
+    </>
+  );
 };
