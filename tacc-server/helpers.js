b49abae9fc5a917a1ba003eb62a1a0ce1896cfe4
hcdiekmann <hcdiekmann@hotmail.com>
2023-09-15 12:49:42 +1200

    add express server for auth

---
+++ tacc-server/helpers.js
@@ -0,0 +1,14 @@
+// This file contains helper functions for the server
+
+const generateRandomString = function (length) {
+  var text = '';
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+export { generateRandomString };
